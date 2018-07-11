346deede40835460a5b71099da1101bde56078d5
3499722281 <3499722281@qq.com>
2018-07-11 20:13:10 +0800

    lj

---
+++ src/main/java/org/java/web/FirstController.java
@@ -0,0 +1,14 @@
+package org.java.web;
+
+/**
+ * Created by 123 on 2018/7/11.
+ * hello world
+ */
+public class FirstController {
+
+
+    public String load(){
+        return  "/ok";
+    }
+
+}
